9d37c48364acd325cd69d0677675f0471f49b19d
JONGBIN-SURFACE <wonjb001@naver.com>
2024-05-31 20:29:28 +0900

    edit retrieve.py - delete document loader: 도큐먼트 로더가 - FIX retrieve db error: test_data를 retrieve에 사용하던 실수를 수정 - 위 변경 사항에 맞게, retrieve_with_query.ipynb를 다시 작성하고, 그에 따른 결과 test_data_with_retrieve.csv도 바뀜.

--- retriever.py
+++ retriever.py
@@ -7,16 +7,6 @@ from transformers import AutoTokenizer
 from rank_bm25 import BM25Okapi
 
 
-class DocumentLoader:
-    def __init__(self, dataframe: pd.DataFrame = None) -> None:
-        self.dataframe = dataframe
-        
-        
-    def load_documents(self, dataframe: pd.DataFrame = None) -> List[str]:
-        dataframe = self.dataframe if dataframe is None else dataframe
-        documents = dataframe['범죄 사실'].tolist()
-        return documents
-
 
 class LongformerTokenizer:
     def __init__(self, model_name: str = 'severinsimmler/xlm-roberta-longformer-base-16384') -> None:
@@ -27,12 +17,13 @@ class LongformerTokenizer:
         return tokens
 
 
+
 class BM25Retriever:
-    def __init__(self, documents: List[str], tokenizer: LongformerTokenizer) -> None:
+    def __init__(self, retrieve_df: pd.DataFrame, tokenizer: LongformerTokenizer) -> None:
         self.tokenizer = tokenizer
-        self.documents = documents
-        self.tokenized_documents = [self.tokenizer.tokenize(doc) for doc in documents]
-        self.bm25 = BM25Okapi(self.tokenized_documents)
+        self.retrieve_df = retrieve_df
+        self.tokenized_facts = [self.tokenizer.tokenize(fact) for fact in retrieve_df['범죄 사실'].tolist()]
+        self.bm25 = BM25Okapi(self.tokenized_facts)
         self.search_history: List[Dict[str, any]] = []
 
 
@@ -51,7 +42,8 @@ class BM25Retriever:
         result = {
             "query": query,
             "results": [{"index": idx, 
-                         "document": self.documents[idx], 
+                         "fact": self.retrieve_df['범죄 사실'][idx],
+                         'laws': self.retrieve_df['법령의 적용'][idx],
                          "score": scores[idx]} 
                         for idx in indices]
         }
@@ -71,7 +63,8 @@ class BM25Retriever:
                         "query_index": query_idx,
                         "query": query,
                         "document_index": result["index"],
-                        "document": " ".join(result["document"]),
+                        "fact": result["fact"],
+                        "laws": result["laws"],
                         "score": result["score"]
                     }
                     records.append(record)
@@ -88,23 +81,23 @@ class BM25Retriever:
         return None
 
 
+
 if __name__ == '__main__':
     
-    df = pd.read_csv("./docs/test_docs.csv")
+    retrieve_df = pd.read_csv("./datasets/train_data.csv")
+    test_df = pd.read_csv("./datasets/test_data_with_query.csv")
     # DocumentLoader 인스턴스 생성 및 문서 로드
-    document_loader = DocumentLoader(df)
-    documents = document_loader.load_documents()
 
     # LongformerTokenizer 인스턴스 생성
     tokenizer = LongformerTokenizer(model_name='severinsimmler/xlm-roberta-longformer-base-16384')
-    tokenized_documnets = [tokenizer.tokenize(document) for document in documents]
+    tokenized_documnets = [tokenizer.tokenize(document) for document in retrieve_df]
 
     # BM25Retriever 인스턴스 생성
-    retriever = BM25Retriever(documents, tokenizer)
+    retriever = BM25Retriever(retrieve_df, tokenizer)
 
     # 쿼리 예제: doc5를 참고해서 음주운전 + 과실치사.
-    sample = df.iloc[4]
-    query_gpt = sample['자연어 쿼리']
+    test_sample = test_df.iloc[4, :]
+    query_gpt = test_sample['자연어 쿼리']
     query_handwritten= """어젯밤에 제가 퇴근하고 동네에서 친구랑 같이 술을 한 잔 했는데요. 시간이 늦어서인지 아무리 기다려도 대리가 안오더라구요.
     그래서 어쩔 수 없이 제가 운전대를 잡고, 후면주차시켰던 차를 뺴려는데, 하필 그때 차 뒤에 저랑 같이 술을 먹던 친구가 있던거에요...
     제가 차를 조금 급하게 빼던 중이라서 친구가 차에 좀 세게 치였고, 지금은 병원에서 입원 중입니다.
